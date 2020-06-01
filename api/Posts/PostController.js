e628d6ae1dd6f15039a6718565bd50ebe8db4f94
Katharina Sachs <katharina.sachs@beuth-hochschule.de>
2020-06-01 13:10:05 +0200

    add createPost and getPosts function

--- api/Posts/PostController.js
+++ api/Posts/PostController.js
@@ -1,6 +1,6 @@
-// UserController.js
+// postController.js
 const express = require('express');
-const router = express.Router();
+var router = express.Router();
 const bodyParser = require('body-parser');
 
 router.use(bodyParser.urlencoded({ extended: true }));
@@ -8,14 +8,22 @@ router.use(bodyParser.json());
 
 const Post = require('./PostModel');
 
-router.get('/', function (req, res) {
-    Post.find({}, function (err, posts) {
+exports.getPosts = function(req, res) {
+    console.log('POST GET')
+    Post.find(function (err, posts) {
         if (err) return res.status(500).send("There was a problem finding the posts.");
         res.status(200).send(posts);
     });
-});
+}
+exports.getPost = function(req, res) {
+    Post.findById(req.params.id, function(err, post) {
+        if (err) return res.status(500).send("There was a problem finding the post.");
+        if (!post) return res.status(404).send("No post found.");
+        res.status(200).send(post);
+    });
+};
 
-router.post('/', function (req, res) {
+exports.createPost = function(req, res) {
     Post.create({
             id : req.body.id,
             name: req.body.name,
@@ -28,35 +36,19 @@ router.post('/', function (req, res) {
             if (err) return res.status(500).send("There was a problem adding the information to the database.");
             res.status(200).send(post);
         });
-});
+}
 
-router.put('/', function (req, res) {
-    Post.update({
-            id : req.body.id,
-            name: req.body.name,
-            email : req.body.email,
-            password : req.body.password,
-            own_posts: req.body.own_posts,
-            starred_posts: req.body.starred_posts
-        },
-        function (err, post) {
-            if (err) return res.status(500).send("There was a problem adding the information to the database.");
-            res.status(200).send(post);
-        });
-});
-router.delete('/', function (req, res) {
-    Post.delete({
-            id : req.body.id,
-            name: req.body.name,
-            email : req.body.email,
-            password : req.body.password,
-            own_posts: req.body.own_posts,
-            starred_posts: req.body.starred_posts
-        },
-        function (err, post) {
-            if (err) return res.status(500).send("There was a problem adding the information to the database.");
-            res.status(200).send(post);
-        });
-});
-
-module.exports = router;
+// router.delete('/:id', function (req, res) {
+//     Post.delete({
+//             id : req.body.id,
+//             name: req.body.name,
+//             email : req.body.email,
+//             password : req.body.password,
+//             own_posts: req.body.own_posts,
+//             starred_posts: req.body.starred_posts
+//         },
+//         function (err, post) {
+//             if (err) return res.status(500).send("There was a problem adding the information to the database.");
+//             res.status(200).send(post);
+//         });
+// });
