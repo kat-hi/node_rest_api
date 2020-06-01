9e9b638e225798bf410f21df4717e65b5be0f1d9
Katharina Sachs <katharina.sachs@beuth-hochschule.de>
2020-06-01 13:13:34 +0200

    outcomment unused lines

--- app.js
+++ app.js
@@ -4,35 +4,37 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
-var UserController = require('./api/User/UserController');
-var PostController = require('./api/Posts/PostController');
+var UserRouter = require('./api/User/UserRoute');
+var PostRouter = require('./api/Posts/PostRoute');
+var IndexRoute = require('./api/Index/IndexRoute')
 
 const app = express();
-app.use('/users', UserController);
-app.use('/posts', PostController);
+app.use('/', IndexRoute);
+app.use('/users', UserRouter);
+app.use('/posts', PostRouter);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.use(logger('dev'));
-app.use(cookieParser());
+// app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+// // catch 404 and forward to error handler
+// app.use(function(req, res, next) {
+//   next(createError(404));
+// });
+//
+// // error handler
+// app.use(function(err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
+//
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render('error');
+// });
 
 module.exports = app;
