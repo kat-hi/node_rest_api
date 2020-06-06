e10f96e5d707b32e61f17e371de7f92f7f38d095
Katharina Sachs <katharina.sachs@beuth-hochschule.de>
2020-06-06 19:20:53 +0200

    add delete and update

--- data/db.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const config = require('config')
-const app = require('app')
-
-const connectionString = config.get('db.connectionString');
-console.log('CONNECTION STRING:', connectionString)
-mongoose.connect(connectionString);
-console.log('db connection established')
-app.log('db connection established')
