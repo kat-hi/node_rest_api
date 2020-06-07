ead3521a3c658e41fe4eed4930fbf3c88da557d4
Katharina Sachs <katharina.sachs@beuth-hochschule.de>
2020-06-07 16:59:25 +0200

    add authentication middleware to mongoose

--- api/User/UsersModel.js
+++ api/User/UsersModel.js
@@ -5,12 +5,29 @@ console.log(config.db.connectionString)
 
 const UserSchema = new mongoose.Schema({
   id: Number,
-  name: String,
-  email: String,
+  name: { type: String, unique: true },
+  email: { type: String, unique: true },
   password: String,
   own_posts: [],
   starred_posts: []
 });
 
-mongoose.model('User', UserSchema);
-module.exports = mongoose.model('User');
+UserSchema.pre('save', function (next) {
+  var user = this;
+  if (!user.isModified('password')) { return next() }
+  bcrypt.hash(user.password,10).then((hashedPassword) => {
+    user.password = hashedPassword;
+    next();
+  })
+}, function (err) {
+  next(err)
+})
+
+UserSchema.methods.comparePassword = function(candidatePassword, next) {
+  bcrypt.compare(candidatePassword, this.password, function (err, isMatch) {
+    if (err)
+      return next(err)
+    next (null, isMatch)
+  })
+}
+module.exports = mongoose.model('User', UserSchema);
