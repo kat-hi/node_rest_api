73aaef7b4d2d2a30b10db1c14225b945aafd5dce
Katharina Sachs <katharina.sachs@beuth-hochschule.de>
2020-06-07 16:58:26 +0200

    refactoring and add deleteUserById

--- api/User/UserController.js
+++ api/User/UserController.js
@@ -1,26 +1,22 @@
 // UserController.js
 const express = require('express');
-const router = express.Router();
-const bodyParser = require('body-parser');
-router.use(bodyParser.urlencoded({ extended: true }));
-router.use(bodyParser.json());
 
 const User = require('./UsersModel');
 
-exports.getUsers = function(req, res) {
-    console.log('USER GET')
+exports.readUsers = async (req, res) => {
     User.find(function (err, users) {
         if (err) return res.status(500).send("There was a problem finding the users.");
         res.status(200).send(users);
     });
 };
-exports.getUser = function(req, res) {
+exports.readUserById = function(req, res) {
     User.findById(req.params.id, function(err, user) {
         if (err) return res.status(500).send("There was a problem finding the user.");
         if (!user) return res.status(404).send("No user found.");
         res.status(200).send(user);
     });
 };
+
 exports.createUser = function(req, res) {
     console.log('USER POST')
     User.create({
@@ -35,18 +31,11 @@ exports.createUser = function(req, res) {
             if (err) return res.status(500).send("There was a problem adding the information to the database.");
             res.status(200).send(user);
         });
+};
+exports.deleteUserById = function (req, res) {
+    User.findByIdAndRemove(req.params.id, function (err, user) {
+        if (err) return res.status(500).send("There was a problem deleting the user.");
+        res.status(200).send("User " + user.name + " was deleted.");
+    });
 }
-// // DELETES A USER FROM THE DATABASE
-// router.delete('/:id', function (req, res) {
-//     User.findByIdAndRemove(req.params.id, function (err, user) {
-//         if (err) return res.status(500).send("There was a problem deleting the user.");
-//         res.status(200).send("User "+ user.name +" was deleted.");
-//     });
-// });
-// // DELETES A USER FROM THE DATABASE
-// router.put('/:id', function (req, res) {
-//     User.update(req.params.id, function (err, user) {
-//         if (err) return res.status(500).send("There was a problem deleting the user.");
-//         res.status(200).send("User "+ user.name +" was updated.");
-//     });
-// });
+
