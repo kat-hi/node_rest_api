25885d768ad17f3c3706d2671b201f2602fb64d9
Katharina Sachs <katharina.sachs@beuth-hochschule.de>
2020-06-07 17:24:12 +0200

    outsource middleware

--- app.js
+++ app.js
@@ -1,18 +1,15 @@
-const createError = require('http-errors');
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const path = require('path');
-const jwt = require('jsonwebtoken')
+const app = express();
 
 var allRoutes = require('./api/allRoutes');
 
-const app = express();
 app.use('/', allRoutes.indexRoute);
 app.use('/users', allRoutes.userRoute);
 app.use('/posts', allRoutes.postRoute);
 
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
@@ -20,44 +17,12 @@ app.use(logger('dev'));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-app.get('/*', function (request, response, next) {
-    if (true) {
-        next();
-    } else {
-        response.send('Sorry, you are not logged in!')
-    }
-})
-app.get('/*', function isAuthenticated(request, response, next) {
-    if (typeof request.headers.authorization !== 'undefined') {
-        let token = request.headers.authorization.split(" ")[1];
-        var privateKey = config.get('session.tokenKey');
-        jwt.very(token, privateKey, { algorithm: "HS256 "}, (err, user) => {
-            if (err) {
-                res.status(500).json({error: "Not Authorized"});
-                return;
-            }
-            return next();
-        });
-    } else {
-        response.status(500).json({ error: "Not Authorized"});
-        return;
-    }
-})
+var security = require('./middleware/security')
+var err = require('./middleware/error')
+app.use(err.errorHandler)
+app.use(err.serverErrorHandler)
+app.get('/*', security.isLoggedIn)
+app.get('/*', security.isAuthenticated)
 
 
 module.exports = app;
