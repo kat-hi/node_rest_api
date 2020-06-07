507dac7dc800318fecd69d7e3f5a53775490076a
Katharina Sachs <katharina.sachs@beuth-hochschule.de>
2020-06-07 16:57:12 +0200

    refactoring and add deleteById

--- api/Posts/PostController.js
+++ api/Posts/PostController.js
@@ -1,54 +1,42 @@
 // postController.js
 const express = require('express');
-var router = express.Router();
 const bodyParser = require('body-parser');
-
-router.use(bodyParser.urlencoded({ extended: true }));
-router.use(bodyParser.json());
 
 const Post = require('./PostModel');
 
-exports.getPosts = function(req, res) {
+exports.readPosts = function(request, response) {
     console.log('POST GET')
-    Post.find(function (err, posts) {
-        if (err) return res.status(500).send("There was a problem finding the posts.");
-        res.status(200).send(posts);
+    Post.find(function (error, posts) {
+        if (error) return response.status(500).send("There was a problem finding the posts.");
+        response.status(200).send(posts);
     });
 }
-exports.getPost = function(req, res) {
-    Post.findById(req.params.id, function(err, post) {
-        if (err) return res.status(500).send("There was a problem finding the post.");
-        if (!post) return res.status(404).send("No post found.");
-        res.status(200).send(post);
+exports.readPostById = function(request, response) {
+    Post.findById(request.params.id, function(error, post) {
+        if (error) return response.status(500).send("There was a problem finding the post.");
+        if (!post) return response.status(404).send("No post found.");
+        response.status(200).send(post);
     });
 };
 
-exports.createPost = function(req, res) {
+exports.createPost = function(request, response) {
     Post.create({
-            id : req.body.id,
-            name: req.body.name,
-            email : req.body.email,
-            password : req.body.password,
-            own_posts: req.body.own_posts,
-            starred_posts: req.body.starred_posts
+            id : request.body.id,
+            name: request.body.name,
+            email : request.body.email,
+            password : request.body.password,
+            own_posts: request.body.own_posts,
+            starred_posts: request.body.starred_posts
         },
-        function (err, post) {
-            if (err) return res.status(500).send("There was a problem adding the information to the database.");
-            res.status(200).send(post);
+        function (error, post) {
+            if (error) return response.status(500).send("There was a problem adding the information to the database.");
+            response.status(200).send(post);
         });
-}
+};
 
-// router.delete('/:id', function (req, res) {
-//     Post.delete({
-//             id : req.body.id,
-//             name: req.body.name,
-//             email : req.body.email,
-//             password : req.body.password,
-//             own_posts: req.body.own_posts,
-//             starred_posts: req.body.starred_posts
-//         },
-//         function (err, post) {
-//             if (err) return res.status(500).send("There was a problem adding the information to the database.");
-//             res.status(200).send(post);
-//         });
-// });
+exports.deletePostById = function (request, response) {
+    Post.findByIdAndRemove(request.params.id, function (error, post) {
+        if (error) return response.status(500).send("There was a problem deleting the post.");
+        response.status(200).send("Post "+ post.name +" was deleted.");
+    });
+}
