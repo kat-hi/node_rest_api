597e700fa27e3598e69d6a2623343940e399eefd
Katharina Sachs <katharina.sachs@beuth-hochschule.de>
2020-06-07 17:00:48 +0200

    outsource all routes in one file to make import easier

---
+++ api/allRoutes.js
@@ -0,0 +1,9 @@
+const userRoute = require('./User/UserRoute')
+const postRoute = require('./Posts/PostRoute')
+const indexRoute = require('./Index/IndexRoute')
+
+module.exports = {
+    userRoute,
+    postRoute,
+    indexRoute
+};
