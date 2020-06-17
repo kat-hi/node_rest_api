3486db9d70331f10c6824fc59d97295140d03862
Katharina Sachs <katharina.sachs@beuth-hochschule.de>
2020-06-18 00:05:57 +0200

    implement ssl when running server in production

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 const app = require('./app');
 const config = require('config')
+const https = require('https');
+const fs = require('fs');
 
-const port = config.get('server.port');
-
-const server = app.listen(port, function() {
-  console.log('Express server runs on http://localhost:'+ port);
-});
+if ( config['env'] === 'production' ) {
+    https.createServer({
+        key: fs.readFileSync('./keytmp.pem'),
+        cert: fs.readFileSync('./cert.pem'),
+    }, app)
+        .listen(config['server'].port, function() {
+            console.log(`Express server runs on ${config['server'].protocol}://localhost:${config['server'].port}`)
+        })
+} else {
+    const server = app.listen(config['server'].port, function() {
+        console.log('Express server runs on http://localhost:'+ config['server'].port)})
+}
