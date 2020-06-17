74ed9c0e689406a08bc5631a763d888c7c4ae321
Katharina Sachs <katharina.sachs@beuth-hochschule.de>
2020-06-18 00:07:24 +0200

    use lambda

--- api/User/UserController.js
+++ api/User/UserController.js
@@ -3,24 +3,21 @@ const express = require('express');
 
 const User = require('./UsersModel');
 
-exports.readUsers = (req, res) => {
-    User.find(function (err, users) {
+exports.readUsers = async (req, res) => {
+    await User.find(function (err, users) {
         if (err) return res.status(500).send("There was a problem finding the users.");
         res.status(200).send(users);
     });
 };
-exports.readUserById = (req, res) => {
-    User.findById(req.params.id, function(err, user) {
+exports.readUserById = async (req, res) => {
+    await User.findById(req.params.id, function(err, user) {
         if (err) return res.status(500).send("There was a problem finding the user.");
         if (!user) return res.status(404).send("No user found.");
         res.status(200).send(user);
     });
 };
 
-exports.createUser = (req, res) => {
-    console.log('USER POST')
-    console.log(req.body)
-    console.log('test')
+exports.createUser = async (req, res) => {
     User.create({
             id : req.body.id,
             name: req.body.name,
@@ -30,13 +27,13 @@ exports.createUser = (req, res) => {
             starred_posts: req.body.starred_posts
         },
         function(err, user) {
-            if (err) return res.status(500).send("There was a problem adding the information to the database." + err );
+            if (err) return res.status(500).send("There was a problem adding the information to the database. " + err );
             res.status(200).send(user);
         });
 };
 exports.deleteUserById = (req, res) => {
     User.findByIdAndRemove(req.params.id, function (err, user) {
-        if (err) return res.status(500).send("There was a problem deleting the user.");
+        if (err) return res.status(500).send("There was a problem deleting the user. " + err);
         res.status(200).send("User " + user.name + " was deleted.");
     });
 }
